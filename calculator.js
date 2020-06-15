755c99e3bc6ccc33b12491647182f1f3a9dd8ada
alsharhanaisha <alsharhanaisha@gmail.com>
2020-06-15 20:40:49 +0300

    Aisha's third submission

--- calculator.js
+++ calculator.js
@@ -1,25 +1,25 @@
 let firstNumber = 3;
 let secondNumber = 5;
-let operation = "+";
+let operation = "*";
 
 if (operation === "+") {
   console.log(
-    `${firstNumber} + ${secondNumber} = `,
-    firstNumber + secondNumber
+    `${firstNumber} + ${secondNumber} = 
+    ${firstNumber + secondNumber}`
   );
 } else if (operation === "-") {
   console.log(
-    `${firstNumber} - ${secondNumber} = `,
-    firstNumber - secondNumber
+    `${firstNumber} - ${secondNumber} = 
+        ${firstNumber - secondNumber}`
   );
 } else if (operation === "*") {
   console.log(
-    `${firstNumber} * ${secondNumber} = `,
-    firstNumber * secondNumber
+    `${firstNumber} * ${secondNumber} = 
+        ${firstNumber * secondNumber}`
   );
 } else {
   console.log(
-    `${firstNumber} / ${secondNumber} = `,
-    firstNumber / secondNumber
+    `${firstNumber} / ${secondNumber} = 
+    ${firstNumber / secondNumber}`
   );
 }
